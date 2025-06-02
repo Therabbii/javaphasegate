ae676897c86374aab694e412531464bba46eeaaa
therabbi.tim@gmail.com <you@example.com>
2025-06-02 15:57:24 +0100

    day 4 subtraction of random numbers

--- DAY 3/IntegerFactors.java
+++ DAY 3/IntegerFactors.java
@@ -9,11 +9,20 @@ public class IntegerFactors{
 
 	while(count <= num){
 		if(number % count == 0)
+	
 		factorCounter ++;
 		count ++;
-
+		int[] factors = new int[count];
+		for(int counter = 0; counter < factors.length; counter++){
+			System.out.print("Element " + (counter + 1) + ":" );
+			numbers[counter] = scanner.nextInt();
+		}
+ 			System.out.println(Arrays.toString(numbers))
+		
 		}
 
+		}
+	
 	return factorCounter;
 
 
---
+++ DAY 4/SubtractRandom.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class SubtractRandom{
+
+	public static int randomGenerator(){
+	Random random = new Random();
+	int number1 = random.nextInt(1 , 100);
+	int number2 = random.nextInt(1 , 100);
+
+	int subtraction = 0;
+
+	
+	if(number1 > number2){
+		subtraction = number1 - number2;
+	System.out.printf("%d - %d" , number1 , number2);
+	}
+	else if  (number1 < number2){
+		int numberLow = number1;
+		subtraction = number2 - numberLow;
+	System.out.printf("%d - %d" , number2 , numberLow);
+	}
+	return subtraction;
+}
+
+
+public static void main(String[] args){
+
+Scanner scanner = new Scanner(System.in);
+
+int count = 0;
+
+for (int attempt = 1; attempt <= 10; attempt++){
+	System.out.printf("Question [%d]:" , attempt);
+	int equationAnswer = randomGenerator();
+	System.out.println("\nWhat is the answer? ");
+	int userInput = scanner.nextInt();
+		if (userInput == equationAnswer){
+		count++;
+		}
+		else if (userInput != equationAnswer){
+		System.out.print("Second try: ");
+		userInput = scanner.nextInt();
+		}
+}
+
+	System.out.printf("==== You scored %d / 10 ====" , count);
+
+}
+
+}
